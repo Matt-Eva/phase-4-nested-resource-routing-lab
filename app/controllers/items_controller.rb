425ccac7a071b75f90bb92df697418bb08bf4222
Matt Eva <matteweva@gmail.com>
2021-12-16 16:28:48 -0800

    commit tree 7bdd3941c08dddcf0fe446f66dd4aa4c6fb0a9c9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,30 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items, status: :ok
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create
+    item = Item.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
